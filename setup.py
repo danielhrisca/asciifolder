d8404059c1bb9fe97d0fc3cfd47fef025376df5c
Daniel Hrisca <daniel.hrisca@gmail.com>
2018-05-21 19:19:13 +0300

    fix license description in setup.py

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         'Topic :: Software Development',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
